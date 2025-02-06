d3df514828a948148206d168b6b6a9a7b76a5444
cb-mohitkumar <mohit.kumar@chargebee.com>
2025-02-06 06:36:01 +0530

    Meals App - Adds basic Nevigation

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,9 +1,16 @@
 import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
-function CategoryGridTile({ title, color }) {
+function CategoryGridTile({ title, color, onPress }) {
   return (
     <View style={[styles.gridItem, { backgroundColor: color }]}>
-      <Pressable android_ripple={{ color: "#ccc" }} style={styles.button}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        onPress={onPress}
+      >
         <View style={styles.innerContainer}>
           <Text style={styles.title}>{title}</Text>
         </View>
