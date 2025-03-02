4b13f5659d6f67b85b6cc7fdc2d61fcb7227ae1e
FailedCommit <mohitchilkoti@gmail.com>
2025-03-02 18:21:00 +0530

    Adds navigators

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 import { Colors } from "./config/colors";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ManageExpenses from "./screens/ManageExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return <BottomTabs.Navigator>
+    <BottomTabs.Screen name="Recent Expenses" component={RecentExpenses} />
+    <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+  </BottomTabs.Navigator>
+}
 
 export default function App() {
   return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello Dear Pratha !</Text>
-      </View>
+    <>
+    <StatusBar style='auto'/>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Expense Overview" component={ExpensesOverview} />
+        <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
