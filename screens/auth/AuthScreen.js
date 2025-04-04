bd7c73b46a58bfcd3d896d132d08c3c893bd7c18
FailedCommit <mohitchilkoti@gmail.com>
2025-04-04 16:52:40 +0530

    Login font change

--- screens/auth/AuthScreen.js
+++ screens/auth/AuthScreen.js
@@ -181,7 +181,13 @@ export default function AuthScreen({ externalError, onGuestLogin }) {
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", padding: 20 },
   card: { padding: 20, elevation: 4 },
-  title: { fontSize: 24, marginBottom: 20, textAlign: "center" },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: "center",
+    fontFamily: "deliusBold",
+    color: Colors.primaryDarkMaroon,
+  },
   input: { marginBottom: 10 },
   button: { marginTop: 10 },
   error: {
