35d5117b28aa228e7683bef793505cee8dceff9c
FailedCommit <mohitchilkoti@gmail.com>
2025-04-17 09:10:51 +0530

    Enables VS Code debugging

--- metro.config.js
+++ metro.config.js
@@ -1,52 +1,34 @@
-// metro.config.js
-const { getDefaultConfig } = require("@expo/metro-config");
+// Learn more https://docs.expo.dev/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
-const config = getDefaultConfig(__dirname);
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// This can be adjusted eg: path.resolve(projectRoot, '..')
+// const workspaceRoot = path.resolve(projectRoot, '../..'); // Uncomment if in a monorepo
 
-// --- START OF ADDED CODE ---
+const config = getDefaultConfig(projectRoot);
 
-// List of font filenames we want to BLOCK (because we only use Ionicons and MaterialIcons)
-const unusedFontFilenames = [
-  "AntDesign.ttf",
-  "Entypo.ttf",
-  "EvilIcons.ttf",
-  "Feather.ttf",
-  "FontAwesome.ttf",
-  "FontAwesome5_Brands.ttf",
-  "FontAwesome5_Regular.ttf",
-  "FontAwesome5_Solid.ttf",
-  "FontAwesome6_Brands.ttf",
-  "FontAwesome6_Regular.ttf",
-  "FontAwesome6_Solid.ttf",
-  "Fontisto.ttf",
-  "Foundation.ttf",
-  "MaterialCommunityIcons.ttf", // The big 1.15MB file we don't use
-  "Octicons.ttf",
-  "SimpleLineIcons.ttf",
-  "Zocial.ttf",
-];
+// --- ADD YOUR CUSTOMIZATIONS HERE ---
+// Example: Enable SVG support
+// const { transformer, resolver } = config;
+// config.transformer = {
+//   ...transformer,
+//   babelTransformerPath: require.resolve('react-native-svg-transformer'),
+// };
+// config.resolver = {
+//   ...resolver,
+//   assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+//   sourceExts: [...resolver.sourceExts, 'svg'],
+// };
 
-// Create regular expression patterns to block these files specifically within the vector-icons path
-// This looks for paths ending like ".../node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/AntDesign.ttf"
-// We escape the dot (.) in the filename to treat it literally.
-const blockListPatterns = unusedFontFilenames.map(
-  (filename) =>
-    new RegExp(
-      `node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/${filename.replace(
-        /\./g,
-        "\\."
-      )}$`
-    )
-);
+// Example: Add workspace roots if in a monorepo
+// config.watchFolders = [workspaceRoot];
+// config.resolver.nodeModulesPaths = [
+//   path.resolve(projectRoot, 'node_modules'),
+//   path.resolve(workspaceRoot, 'node_modules'),
+// ];
 
-// Ensure the resolver and blockList exist, then add our patterns
-config.resolver = config.resolver || {};
-// config.resolver.blockList = config.resolver.blockList
-//   ? Array.isArray(config.resolver.blockList)
-//     ? [...config.resolver.blockList, ...blockListPatterns] // Add to existing array
-//     : [config.resolver.blockList, ...blockListPatterns] // Wrap existing non-array blockList
-//   : blockListPatterns; // Create blockList if it doesn't exist
-
-// --- END OF ADDED CODE ---
+// --- END CUSTOMIZATIONS ---
 
 module.exports = config;
