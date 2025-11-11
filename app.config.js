5207084b64ede7f6fed9633545dd8a2c72b1e451
FailedCommit <mohitchilkoti@gmail.com>
2025-11-12 02:47:53 +0530

    Increment path version for release

--- app.config.js
+++ app.config.js
@@ -27,7 +27,7 @@ export default ({ config }) => {
 
   const majorVersion = 20;
   const minorVersion = 0;
-  const patchVersion = 6;
+  const patchVersion = 11;
   const buildIteration = 0; // Or 1 if you prefer to start iterations from 1
 
   const androidVersionCode =
